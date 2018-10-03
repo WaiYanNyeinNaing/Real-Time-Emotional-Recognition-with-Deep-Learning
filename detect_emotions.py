0db9c502a2b47985611bba9928e9800b0fcf8792
WaiYanNyeinNaing <31321485+WaiYanNyeinNaing@users.noreply.github.com>
2018-10-04 04:05:50 +0630

    Add files via upload

---
+++ detect_emotions.py
@@ -0,0 +1,69 @@
+from keras.preprocessing.image import img_to_array
+import imutils
+import cv2
+from keras.models import load_model
+import numpy as np
+import pyttsx3
+import pyaudio
+
+
+# parameters for loading data and images
+detection_model_path = 'haarcascade/haarcascade_frontalface_default.xml'
+emotion_model_path = 'pretrained_models/cnn.hdf5'
+
+# hyper-parameters for bounding boxes shape
+# loading models
+face_detection = cv2.CascadeClassifier(detection_model_path)
+emotion_classifier = load_model(emotion_model_path, compile=False)
+EMOTIONS = ["angry" ,"disgust","scared", "happy", "sad", "surprised",
+ "neutral"]
+
+feelings_faces = []
+
+camera = cv2.VideoCapture(0)
+while True:
+    frame = camera.read()[1]
+    #reading the frame
+    frame = imutils.resize(frame,width=800)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_detection.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30,30),flags=cv2.CASCADE_SCALE_IMAGE)
+    
+    canvas = np.zeros((250, 300, 3), dtype="uint8")
+    frameClone = frame.copy()
+    if len(faces) > 0:
+        faces = sorted(faces, reverse=True,
+        key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
+        (X, Y, W, H) = faces
+
+        # Extract the facial key point of the face from the grayscale image, resize it to a fixed 28x28 pixels, and then prepare
+        # the facial for classification via the CNN
+        facial = gray[Y:Y + H, X:X + W]
+        facial = cv2.resize(facial, (64, 64))
+        facial = facial.astype("float") / 255.0
+        facial = img_to_array(facial)
+        facial = np.expand_dims(facial, axis=0)
+        
+        
+        preds = emotion_classifier.predict(facial)[0]
+        emotion_probability = np.max(preds)
+        label = EMOTIONS[preds.argmax()]
+
+    for (i, (emotion, prob)) in enumerate(zip(EMOTIONS, preds)):
+                # construct the label text
+                text = "{}: {:.2f}% ".format(emotion, prob * 100)       
+                w = int(prob * 300)
+                cv2.rectangle(canvas, (7, (i * 35) + 5),
+                (w, (i * 35) + 35), (255, 0, 0), -1)
+                cv2.putText(frameClone,label,(X, Y - 30),  
+                cv2.FONT_HERSHEY_DUPLEX, 0.9, (0, 0, 255), 2)
+                cv2.rectangle(frameClone, (X, Y), (X + W, Y + H),
+                              (255, 0, 0), 2)
+                
+                
+    cv2.imshow('Emotion Status', frameClone)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
---
+++ train.py
@@ -0,0 +1,170 @@
+#Preprocessing
+from keras.preprocessing.image import ImageDataGenerator
+from load_dataset import load_fer2013
+from load_dataset import preprocess_input
+from sklearn.model_selection import train_test_split
+#Model Training
+from keras.layers import Activation, Convolution2D, Dropout, Conv2D
+from keras.layers import AveragePooling2D, BatchNormalization
+from keras.layers import GlobalAveragePooling2D
+from keras.models import Sequential
+from keras.layers import Flatten
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import MaxPooling2D
+from keras.layers import SeparableConv2D
+from keras import layers
+from keras.regularizers import l2
+#Save Model
+from keras.callbacks import CSVLogger, ModelCheckpoint, EarlyStopping
+from keras.callbacks import ReduceLROnPlateau
+
+
+# parameters
+batch_size = 32
+num_epochs = 10000
+input_shape = (48, 48, 1)
+validation_split = .2
+verbose = 1
+num_classes = 7
+patience = 50
+
+#path to save the models
+base_path = 'models/'                         
+
+# data generator
+data_generator = ImageDataGenerator(
+                        featurewise_center=False,
+                        featurewise_std_normalization=False,
+                        rotation_range=10,
+                        width_shift_range=0.1,
+                        height_shift_range=0.1,
+                        zoom_range=.1,
+                        horizontal_flip=True)
+
+
+def cnn(input_shape, num_classes, l2_regularization=0.01):
+    regularization = l2(l2_regularization)
+
+    # base
+    img_input = Input(input_shape)
+    x = Conv2D(8, (3, 3), strides=(1, 1), kernel_regularizer=regularization,
+                                            use_bias=False)(img_input)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(8, (3, 3), strides=(1, 1), kernel_regularizer=regularization,
+                                            use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    # module 1
+    residual = Conv2D(16, (1, 1), strides=(2, 2),
+                      padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = SeparableConv2D(16, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SeparableConv2D(16, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+
+    # module 2
+    residual = Conv2D(32, (1, 1), strides=(2, 2),
+                      padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = SeparableConv2D(32, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SeparableConv2D(32, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+
+    # module 3
+    residual = Conv2D(64, (1, 1), strides=(2, 2),
+                      padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = SeparableConv2D(64, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SeparableConv2D(64, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+
+    # module 4
+    residual = Conv2D(128, (1, 1), strides=(2, 2),
+                      padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = SeparableConv2D(128, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SeparableConv2D(128, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+
+    x = Conv2D(num_classes, (3, 3),
+            #kernel_regularizer=regularization,
+            padding='same')(x)
+    x = GlobalAveragePooling2D()(x)
+    output = Activation('softmax',name='predictions')(x)
+
+    model = Model(img_input, output)
+    return model
+
+# model parameters/compilation
+model = cnn(input_shape, num_classes)
+model.compile(optimizer='adam', loss='categorical_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+
+# callbacks
+log_file_path = base_path + '_emotion_training.log'
+csv_logger = CSVLogger(log_file_path, append=False)
+early_stop = EarlyStopping('val_loss', patience=patience)
+reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1,
+                                  patience=int(patience/4), verbose=1)
+trained_models_path = base_path + '_cnn'
+model_names = trained_models_path + '.{epoch:02d}-{val_acc:.2f}.hdf5'
+model_checkpoint = ModelCheckpoint(model_names, 'val_loss', verbose=1,
+                                                    save_best_only=True)
+callbacks = [model_checkpoint, csv_logger, early_stop, reduce_lr]
+
+# loading dataset
+faces, emotions = load_fer2013()
+faces = preprocess_input(faces)
+num_samples, num_classes = emotions.shape
+xtrain, xtest,ytrain,ytest = train_test_split(faces, emotions,test_size=0.2,shuffle=True)
+
+#Train the model
+model.fit_generator(data_generator.flow(xtrain, ytrain,
+                                            batch_size),
+                        steps_per_epoch=len(xtrain) / batch_size,
+                        epochs=num_epochs, verbose=1, callbacks=callbacks,
+                        validation_data=(xtest,ytest))
